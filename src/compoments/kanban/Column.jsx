79fc2e735e823bab286030ab0581406648cc858e
Dent <dent_yuan@17life.com>
2024-03-07 16:52:15 +0800

    多欄DnD

--- src/compoments/kanban/Column.jsx
+++ src/compoments/kanban/Column.jsx
@@ -3,12 +3,12 @@ import Item from './Item'
 
 import kanbanCss from '../../assets/scss/kanban.module.scss'
 
-const Column = ({ list }) => {
+const Column = ({ col: { id, list } }) => {
   return (
-    <Droppable droppableId='col-1'>
+    <Droppable droppableId={id}>
       {(provided) => (
         <div className={kanbanCss.column}>
-          <h2>清單一</h2>
+          <h2>{id}</h2>
           <div className={kanbanCss.list} {...provided.droppableProps} ref={provided.innerRef}>
             {list.map((text, index) => (
               <Item key={text} text={text} index={index} />
--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -7,17 +7,59 @@ import Column from '../compoments/kanban/Column'
 
 const { Title } = Typography;
 
-const Kanban = () => {
-  const [list, setList] = useState(['Item 1', 'Item 2', 'Item 3'])
+const initialColumns = {
+  todo: {
+    id: 'todo',
+    list: ['item 1', 'item 2', 'item 3']
+  },
+  doing: {
+    id: 'doing',
+    list: []
+  },
+  done: {
+    id: 'done',
+    list: []
+  }
+}
 
-  const onDragEnd = () => null
+const Kanban = () => {
+  const [columns, setColumns] = useState(initialColumns)
+
+  const onDragEnd = ({ source, destination }) => {
+    if (destination === undefined || destination === null) return null
+
+    if (destination.droppableId === source.droppableId && destination.index === source.index) return null
+
+    const start = columns[source.droppableId]
+    const end = columns[destination.droppableId]
+
+    if (start === end) {
+      const newList = start.list.filter((_, idx) => idx !== source.index)
+      newList.splice(destination.index, 0, start.list[source.index])
+      const newCol = { id: start.id, list: newList }
+
+      setColumns(state => ({ ...state, [newCol.id]: newCol }))
+    } else {
+      const newStartList = start.list.filter((_, idx) => idx !== source.index)
+      const newStartCol = { id: start.id, list: newStartList }
+
+      const newEndList = end.list
+      newEndList.splice(destination.index, 0, start.list[source.index])
+      const newEndCol = { id: end.id, list: newEndList }
+
+      setColumns(state => ({ ...state, [newStartCol.id]: newStartCol, [newEndCol.id]: newEndCol }))
+    }
+    return null
+  }
 
   return (
     <div>
       <Title level={3}>看板</Title>
       <DragDropContext onDragEnd={onDragEnd}>
         <div className={kanbanCss.kanban}>
-          <Column list={list} />
+        {Object.values(columns).map(col => (
+          <Column col={col} key={col.id} />
+        ))}
         </div>
       </DragDropContext>
     </div>
